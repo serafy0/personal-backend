298f4c9a3fd034c843e3a07e2cf80122c2eff258
mohamedelserafy <elserafy02@gmail.com>
2022-05-17 00:48:13 +0200

    rate limiter

--- server.ts
+++ server.ts
@@ -1,12 +1,21 @@
 import express, { Express, Request, Response } from "express"
 import helmet from "helmet"
+import rateLimit, { RateLimitRequestHandler } from "express-rate-limit"
 import dotenv from "dotenv"
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
 
+const limiter: RateLimitRequestHandler = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+})
+
 app.use(helmet())
+app.use(limiter)
 
 app.all("*", (req: Request, res: Response) => {
     res.status(404).json({ message: "Not Found" })
