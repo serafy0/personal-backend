c9ad65cca6afad1feb23f696231e61615807f2d2
mohamedelserafy <elserafy02@gmail.com>
2022-05-18 23:57:44 +0200

    projects table

---
+++ db/migrations/20220518025514_projects.ts
@@ -0,0 +1,29 @@
+import { table } from "console"
+import { Knex } from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("projects", (table) => {
+        table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"))
+        table.uuid("title").references("string_contents.id").notNullable()
+        table.string("image").notNullable()
+    })
+    await knex.schema.createTable("links", (table) => {
+        table.string("link").primary()
+        table.uuid("project").references("projects.id").notNullable()
+        table.string("title").notNullable()
+    })
+    await knex.schema.createTable("tags", (table) => {
+        table.string("title").primary()
+    })
+    await knex.schema.createTable("projects_tags", (table) => {
+        table.uuid("project_id").references("projects.id")
+        table.string("tag").references("tags.title")
+    })
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists("links")
+    await knex.schema.dropTableIfExists("projects_tags")
+    await knex.schema.dropTableIfExists("tags")
+    await knex.schema.dropTableIfExists("projects")
+}
