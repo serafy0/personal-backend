84dca4c4e970710ec6eeb37aa463fdec2a86cc63
mohamedelserafy <elserafy02@gmail.com>
2022-05-21 03:54:45 +0200

    add interface to project mapper

--- components/projects/projects.mapper.ts
+++ components/projects/projects.mapper.ts
@@ -1,14 +1,13 @@
-export function hydrateProjects(projects: any[]): any[] {
-    const map = new Map()
+export function hydrateProjects(projects: any[]): Project[] {
+    const map = new Map<string, Project>()
     for (let project of projects) {
         const { id, link } = project
         delete project["link"]
-
-        if (map.has(id)) {
-            map.set(id, {
-                ...project,
-                links: [...map.get(id).links, link],
-            })
+        const item = map.get(id)
+        if (item) {
+            const { links } = item
+            links.push(link)
+            map.set(id, { ...project, links })
         } else {
             map.set(id, { ...project, links: [link] })
         }
