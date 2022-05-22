eaa229574a7c4006fd0e313b0c55f2a554d0c1fe
mohamedelserafy <elserafy02@gmail.com>
2022-05-22 12:22:22 +0200

    refactor project mapper

--- components/projects/projects.mapper.ts
+++ components/projects/projects.mapper.ts
@@ -1,15 +1,26 @@
 export function hydrateProjects(projects: any[]): Project[] {
     const map = new Map<string, Project>()
     for (let project of projects) {
-        const { id, link } = project
-        delete project["link"]
+        const { id, link, link_title, title, image } = project
+        let hydratedLink: Link = { url: link, title: link_title }
         const item = map.get(id)
         if (item) {
             const { links } = item
-            links.push(link)
-            map.set(id, { ...project, links })
+            links.push({ url: hydratedLink.url, title: hydratedLink.title })
+
+            map.set(id, {
+                id,
+                title,
+                image,
+                links: links,
+            })
         } else {
-            map.set(id, { ...project, links: [link] })
+            map.set(id, {
+                id,
+                title,
+                image,
+                links: [hydratedLink],
+            })
         }
     }
     return Array.from(map.values())
